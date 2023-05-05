414e29b79c148d30ec5f980bf7c96c1dc8959188
Alex Charlton <alex.n.charlton@gmail.com>
2023-05-04 17:25:40 -0700

    Update baseview

--- nih-plug/examples/hello/src/main.rs
+++ nih-plug/examples/hello/src/main.rs
@@ -1,4 +1,6 @@
-#![windows_subsystem = "windows"]
+// Uncomment to remove the terminal window associated with the application on Windows
+// Will also disable printing to a terminal.
+// #![windows_subsystem = "windows"]
 use lemna_nih_plug::nih_plug::prelude::*;
 
 use plugin_example_hello::HelloPlugin;
